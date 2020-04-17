4d8ed0af04685cde6eb231abd0c0d84503f92e74
Jefferson Corrêa Braga <jefferson.braga.correa@gmail.com>
2020-04-17 11:33:40 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  };
+  
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+  
+  let repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+   
+  repository = { ...repository, url, title, techs, };
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.findIndex(repository => repository.id === id);
+
+  if(repository < 0) {
+     return response.status(400).send();
+  }
+  
+  repositories.splice(repository, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+  
+  return response.json(repository);
 });
 
 module.exports = app;
